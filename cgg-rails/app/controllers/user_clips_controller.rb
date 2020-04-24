821f4b5e96f939af0a620cd47d03a349f715497d
mattjagiello <mjagiello@gmail.com>
2020-04-23 19:20:44 -0500

    Added rails resources

---
+++ cgg-rails/app/controllers/user_clips_controller.rb
@@ -0,0 +1,2 @@
+class UserClipsController < ApplicationController
+end
---
+++ cgg-rails/db/migrate/20200424001925_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :post_name
+      t.string :post_url
+      t.integer :post_upvotes
+
+      t.timestamps
+    end
+  end
+end
