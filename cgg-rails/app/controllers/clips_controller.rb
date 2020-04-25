4d13f169ac40487a88c4286a27823a351ece3fb1
mattjagiello <mjagiello@gmail.com>
2020-04-25 14:04:29 -0500

    Added associations for user and clips

--- cgg-rails/app/controllers/clips_controller.rb
+++ cgg-rails/app/controllers/clips_controller.rb
@@ -1,2 +1,6 @@
 class ClipsController < ApplicationController
+    belongs_to :post
+    has_many :user_clips
+    has_many :comments, through: :posts
+    has_many :users, through: :user_clips
 end
--- cgg-rails/app/models/user.rb
+++ cgg-rails/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :user_clips
+    has_many :clips, through: :user_clips
 end
