0def8bc2c780c0bee067979b97a47f96de17d802
mattjagiello <mjagiello@gmail.com>
2020-04-27 13:35:41 -0500

    Testing API response

--- cgg-rails/app/controllers/posts_controller.rb
+++ cgg-rails/app/controllers/posts_controller.rb
@@ -19,7 +19,27 @@ class PostsController < ApplicationController
         request["authorization"] = 'Basic UEtHUUlPWThkT2dtLVE6RV84TzcxRDY5N0gyd1JDNXNBLXpHWUlwV1dF'
         
         response = http.request(request)
-        puts response.read_body
+        api_key = response.read_body.slice(18,40)
+        # binding.pry
+        subreddit_url = URI("https://oauth.reddit.com/r/LivestreamFail/top/")
+
+        http = Net::HTTP.new(subreddit_url.host, subreddit_url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Get.new(subreddit_url)
+        request["cookie"] = 'loid=00000000003yiyjymz.2.1560752627799.Z0FBQUFBQmVwZWI3TUh4Qlo1REFEMk9uMDNTTG5tVmVKb1o0R0hObWotaXRITmZ0bEs4NWVrT0hXdTcwVk1NS1M2bUNFbWt0eG9uMFZ1dVphODBVOUFiQnVIVDF0WFZ0X1dIclBMQVhLbG1YdXh3MEtXeVlWUndBUHdLU3hxWktBeFpEZlJZR3FYZjE; edgebucket=Gsf4x7jxWRGKk056KD; csv=1'
+        request["user-agent"] = 'Flatiron final project by /u/thundershirtatx'
+        request["authorization"] = 'Bearer' + api_key
+
+        response = http.request(request)
+        posts_string = response.read_body
+        p posts_extract = URI.extract(response.body)
+        # p posts_string
+
+        # posts_string = response.body.to_s
+        # p posts_string.force_encoding( Encoding::UTF_8 )
+        binding.pry
     end
 
 end
