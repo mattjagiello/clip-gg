f13025b66f11e46cdbfb3b65ae52ad2819eea3ba
mattjagiello <mjagiello@gmail.com>
2020-04-27 11:58:15 -0500

    Added pry testing API

--- cgg-rails/app/controllers/posts_controller.rb
+++ cgg-rails/app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
+require 'open-uri'
+require 'net/http'
+require 'json'
+require 'pry'
+
 class PostsController < ApplicationController
+
+    def index
+    url = 'https://oauth.reddit.com/r/livestreamfail/top'
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    binding.pry
+    end
+
+
 end
