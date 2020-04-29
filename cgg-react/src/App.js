9afe039b224a6a444f18934b7e69f550c4a8e820
mattjagiello <mjagiello@gmail.com>
2020-04-29 14:49:38 -0500

    Setup App.js file and main components

--- cgg-react/src/App.js
+++ cgg-react/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js';
+import ClipContainer from './components/ClipContainer.js';
+import Clip from './components/Clip.js';
+import SaveClip from './components/SaveClip.js';
+import CommentContainer from './components/CommentContainer.js';
+import Comment from './components/Comment.js';
+import UserContainer from './components/UserContainer.js';
+import User from './components/User.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NavBar />
+        <ClipContainer />
+        <Clip />
+        <SaveClip />
+        <CommentContainer />
+        <Comment />
+        <UserContainer />
+        <User />
     </div>
   );
 }
