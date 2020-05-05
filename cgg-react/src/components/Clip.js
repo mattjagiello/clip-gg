d326cec9ad93699bd7368d851a2034a4d5c90d04
mattjagiello <mjagiello@gmail.com>
2020-05-05 03:44:53 -0500

    testing in clip component

--- cgg-react/src/components/Clip.js
+++ cgg-react/src/components/Clip.js
@@ -14,19 +14,25 @@ class Clip extends Component {
         return (
             <div className="Clip-Main">
                 <header>
-                    <p><iframe 
-                        src={"https://clips.twitch.tv/embed?clip="+ this.props.slug}
-                        
-                        // TODO - Accept props of url for clip
-                        // frameborder="0"
-                        // allowfullscreen="true"
-                        height="378"
-                        width="620"
-                        title={this.props.currentClip.post_name}
+                    <div id="twitch-embed">
+                    <script src="https://embed.twitch.tv/embed/v1.js"></script>
+
+                        <iframe
+                            // src={"https://clips.twitch.tv/embed?clip=" + this.props.slug + "&parent=clipgg.heroku.com"}
+                            src={"https://clips.twitch.tv/embed?clip=" + this.props.slug + "&parent=streamernews.example.com&parent=embed.example.com"}
+                            // TODO - Accept props of url for clip
+                            // frameborder="0"
+                            // allowfullscreen="true"
+                            height="378"
+                            width="620"
+                            title={this.props.currentClip.post_name}
+                            preload="auto"
+                            parent="http://localhost/"
                         >
-                            </iframe></p>
-                </header>
-            </div>
+                        </iframe>
+                    </div>
+                </header >
+            </div >
         )
     }
 }
