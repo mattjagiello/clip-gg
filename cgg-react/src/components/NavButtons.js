d2732a205ca67bc3f24e958ae96c6c9831dea7d9
mattjagiello <mjagiello@gmail.com>
2020-05-07 03:41:09 -0500

    Modified initial CSS

--- cgg-react/src/components/NavButtons.js
+++ cgg-react/src/components/NavButtons.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../NavButtons.css';
 
 class NavButtons extends Component {
 
@@ -12,8 +13,10 @@ class NavButtons extends Component {
     render() {
         return (
             <div className="navbuttons">
-                <p><input type="button" onClick={this.props.prevClip} />
-                    <input type="button" onClick={this.props.nextClip} /></p>
+                <p>
+                    <input className="example_c" type="button" onClick={this.props.prevClip} value="Previous Clip" />
+                    <input className="example_c" type="button" onClick={this.props.nextClip} value="Next Clip" />
+                </p>
             </div>
         )
     }
