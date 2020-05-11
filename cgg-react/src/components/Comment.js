2fa5b0b754d1591216ea99aa7d410173b9d940fd
mattjagiello <mjagiello@gmail.com>
2020-05-11 13:59:54 -0500

    MVP updates

--- cgg-react/src/components/Comment.js
+++ cgg-react/src/components/Comment.js
@@ -2,31 +2,24 @@ import React, { Component } from 'react';
 import '../Comment.css';
 
 class Comment extends Component {
+	constructor() {
+		super();
+		this.state = {
+			curUser: ''
+		};
+	}
 
-    constructor() {
-        super()
-        this.state = {
-            curUser: '',
-        }
-    }
-
-    render() {
-        return (
-            <div className="Comment-Main">
-                <ol>
-                    <li>
-                        {this.props.comments ? this.props.comments[this.props.counter] : null }
-                    </li>
-                    <li>
-                        {this.props.comments ? this.props.comments[this.props.counter]  : null }
-                    </li>
-                    <li>
-                        {this.props.comments ? this.props.comments[this.props.counter]  : null }
-                    </li>
-                </ol>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="Comment-Main">
+				<ol>
+					<li>{this.props.comments ? this.props.comments[this.props.counter] : null}</li>
+					<li>{this.props.comments ? this.props.comments[this.props.counter] : null}</li>
+					<li>{this.props.comments ? this.props.comments[this.props.counter] : null}</li>
+				</ol>
+			</div>
+		);
+	}
 }
 
-export default Comment    
+export default Comment;
--- cgg-react/src/components/CommentContainer.js
+++ cgg-react/src/components/CommentContainer.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import Comment from './Comment.js';
 
 class CommentContainer extends Component {
+	constructor() {
+		super();
+		this.state = {
+			curUser: '',
+			comments: [
+				'Single mother flees from violently autistic son.',
+				'Clearly Mizkif abuses her and she has PTSD and his actions triggered her reaction',
+				"I am Mizkif's ex-GF. I can confirm he used to abuse me daily, it was his version of 'quality time'. I am still doing my best at recovering from this dreadful period of my life. But to be fair, he had a pretty good reason to get upset here."
+			]
+		};
+	}
 
-    constructor() {
-        super()
-        this.state = {
-            curUser: '',
-            comments: ['No comment data found','No comment data found','No comment data found']
-        }
-    }
-
-    render() {
-        return (
-            <div className="CommentContainer-Main">
-                <header>
-                <Comment 
-                comments = {this.state.comments}
-                counter={this.props.counter}
-                />
-                </header>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="CommentContainer-Main">
+				<header>
+					<Comment comments={this.state.comments} counter={this.props.counter + 1} />
+				</header>
+			</div>
+		);
+	}
 }
 
-export default CommentContainer  
+export default CommentContainer;
